033d48dce27576eb8977f92168be041b25978b65
MandLisa <lisa.mandl@tum.de>
2024-07-07 18:01:00 +0200

    add year and NDVI trajectory

--- analysis_files/add_year.R
+++ analysis_files/add_year.R
@@ -1,5 +1,7 @@
 # Load necessary library
 library(dplyr)
+library(ggplot2)
+library(zoo)
 
 # Set the directory containing the files
 setwd("/data/eo/EO4Alps/level3/NDVI/features")
@@ -53,5 +55,68 @@ write.table(features_combined, "combined_features.csv", row.names = FALSE, col.n
 
 # View the combined data frame (optional)
 print(combined_data)
+
+
+# Read the data from CSV file
+combined_features <- read.csv("/data/eo/EO4Alps/level3/NDVI/combined_features.csv")
+
+# Create the ID column based on unique combinations of X and Y
+combined_features <- combined_features %>%
+  group_by(X, Y) %>%
+  mutate(ID = cur_group_id()) %>%
+  ungroup()
+
+# write
+write.csv(combined_features, "/data/eo/EO4Alps/level3/NDVI/combined_features.csv", row.names = FALSE)
+
+# Remove negative values and set them to NA for interpolation
+combined_features_clean <- combined_features %>%
+  mutate(value = ifelse(value < 0, NA, value))
+
+# Perform linear interpolation by ID
+combined_features_clean <- combined_features_clean %>%
+  group_by(ID) %>%
+  mutate(value = na.approx(value, na.rm = FALSE)) %>%
+  ungroup()
+
+
+# Filter out IDs with negative values
+combined_features_clean <- combined_features %>%
+  group_by(ID) %>%
+  filter(all(value >= 0)) %>%
+  ungroup()
+
+
+
+
+# plot
+ggplot(combined_features_clean, aes(x = year, y = value, group = ID, color = as.factor(ID))) + 
+  geom_line() + 
+  facet_wrap(~ class) + 
+  theme_minimal() + 
+  labs(x = "Year", y = "Value", title = "Values Over Time by Class and ID") +
+  theme(legend.position = "none")  # Optionally remove legend if there are many IDs
+
+
+summary_stats <- combined_features_clean %>%
+  group_by(class) %>%
+  summarise(mean_value = mean(value, na.rm = TRUE),
+            sd_value = sd(value, na.rm = TRUE)) %>%
+  ungroup()
+
+
+# Plot the cleaned data with mean and ribbon
+ggplot(combined_features_clean, aes(x = year, y = value, group = ID, color = as.factor(ID))) + 
+  geom_line(alpha = 0.5) +  # Add lines for individual IDs with transparency
+  geom_line(data = summary_stats, aes(y = mean_value, group = class), color = "black", size = 1.5) +  # Add mean line per class
+  #geom_ribbon(data = summary_stats, aes(ymin = mean_value - sd_value, ymax = mean_value + sd_value, group = class), alpha = 0.2) +  # Add ribbon for variability
+  facet_wrap(~ class) + 
+  theme_minimal() + 
+  labs(x = "Year", y = "Value", title = "Values Over Time with Mean and Variability by Class") +
+  theme(legend.position = "none")
+
+
+
+
 
 
