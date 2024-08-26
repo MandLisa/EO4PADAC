b453661b99a6acab75cd1196f2ed668122b05677
MandLisa <lisa.mandl@tum.de>
2024-08-26 12:09:46 +0000

    update

--- analysis_files/recovery_analysis/recovery_x_climate.R
+++ analysis_files/recovery_analysis/recovery_x_climate.R
@@ -306,37 +306,204 @@ recovery_VPD_long <- recovery_VPD %>%
 ### write
 write.csv(recovery_VPD_long, "~/eo_nas/EO4Alps/00_analysis/_recovery/recovery_2608.csv", row.names=FALSE)
 
-# Create a new DataFrame with the filtered values
-recovery_VPD_long1 <- recovery_VPD_long %>%
-  filter(recovery_rate <= 39)
 
-# unique
-recovery_VPD_unique <- recovery_VPD_long1  %>%
+recovery_2608 <- recovery_2608 %>% select(-VPD_yod, -VPD_yod1, -VPD_yod2, -severity_absolute,
+                                        -tree_share_80, -month)
+
+
+recovery_2608_1 <- recovery_2608 %>%
+  group_by(ID_new) %>%                              # Group the data by ID
+  mutate(
+    VPD_absolute_yod = ifelse(ysd == 0, VPD_absolute, NA),
+    VPD_absolute_yod1 = ifelse(ysd == 1, VPD_absolute, NA),
+    VPD_absolute_yod2 = ifelse(ysd == 2, VPD_absolute, NA),
+    VPD_absolute_yod3 = ifelse(ysd == 3, VPD_absolute, NA),
+    VPD_absolute_yod4 = ifelse(ysd == 4, VPD_absolute, NA),
+    VPD_absolute_yod5 = ifelse(ysd == 5, VPD_absolute, NA)
+  ) %>%
+  ungroup()    
+
+
+recovery_2608_1 <- recovery_2608 %>%
+  group_by(ID_new) %>%  # Group by ID
+  mutate(
+    VPD_absolute_yod0 = case_when(ysd == 0 ~ VPD_absolute),
+    VPD_absolute_yod1 = case_when(ysd == 1 ~ VPD_absolute),
+    VPD_absolute_yod2 = case_when(ysd == 2 ~ VPD_absolute),
+    VPD_absolute_yod3 = case_when(ysd == 3 ~ VPD_absolute),
+    VPD_absolute_yod4 = case_when(ysd == 4 ~ VPD_absolute),
+    VPD_absolute_yod5 = case_when(ysd == 5 ~ VPD_absolute)
+  ) %>%
+  ungroup()
+
+
+str(recovery_2608)
+head(recovery_2608)
+
+
+recovery_2608_test <- recovery_2608 %>%
+  filter(year == yod) %>%  # Filter for rows where year equals yod
+  select(ID_new, VPD_absolute) %>%  # Select necessary columns
+  rename(VPD_absolute_yod = VPD_absolute)
+
+# Now, join this back with the original data frame
+recovery_2608_1 <- recovery_2608 %>%
+  left_join(recovery_2608_test, by = "ID_new")
+
+recovery_2608_test <- recovery_2608 %>%
+  filter(year == yod+1) %>%  # Filter for rows where year equals yod
+  select(ID_new, VPD_absolute) %>%  # Select necessary columns
+  rename(VPD_absolute_yod1 = VPD_absolute)
+
+# Now, join this back with the original data frame
+recovery_2608_1 <- recovery_2608_1 %>%
+  left_join(recovery_2608_test, by = "ID_new")
+
+recovery_2608_test <- recovery_2608 %>%
+  filter(year == yod+2) %>%  # Filter for rows where year equals yod
+  select(ID_new, VPD_absolute) %>%  # Select necessary columns
+  rename(VPD_absolute_yod2 = VPD_absolute)
+
+# Now, join this back with the original data frame
+recovery_2608_1 <- recovery_2608_1 %>%
+  left_join(recovery_2608_test, by = "ID_new")
+
+
+recovery_2608_test <- recovery_2608 %>%
+  filter(year == yod+3) %>%  # Filter for rows where year equals yod
+  select(ID_new, VPD_absolute) %>%  # Select necessary columns
+  rename(VPD_absolute_yod3 = VPD_absolute)
+
+# Now, join this back with the original data frame
+recovery_2608_1 <- recovery_2608_1 %>%
+  left_join(recovery_2608_test, by = "ID_new")
+
+
+recovery_2608_test <- recovery_2608 %>%
+  filter(year == yod+4) %>%  # Filter for rows where year equals yod
+  select(ID_new, VPD_absolute) %>%  # Select necessary columns
+  rename(VPD_absolute_yod4 = VPD_absolute)
+
+# Now, join this back with the original data frame
+recovery_2608_1 <- recovery_2608_1 %>%
+  left_join(recovery_2608_test, by = "ID_new")
+
+
+recovery_2608_test <- recovery_2608 %>%
+  filter(year == yod+5) %>%  # Filter for rows where year equals yod
+  select(ID_new, VPD_absolute) %>%  # Select necessary columns
+  rename(VPD_absolute_yod5 = VPD_absolute)
+
+# Now, join this back with the original data frame
+recovery_2608_1 <- recovery_2608_1 %>%
+  left_join(recovery_2608_test, by = "ID_new")
+
+
+
+# Rename columns
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_autumn_yod+1"] <- "VPD_autumn_yod1"
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_autumn_yod+2"] <- "VPD_autumn_yod2"
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_autumn_yod+3"] <- "VPD_autumn_yod3"
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_autumn_yod+4"] <- "VPD_autumn_yod4"
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_autumn_yod+5"] <- "VPD_autumn_yod5"
+
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_spring_yod+1"] <- "VPD_spring_yod1"
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_spring_yod+2"] <- "VPD_spring_yod2"
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_spring_yod+3"] <- "VPD_spring_yod3"
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_spring_yod+4"] <- "VPD_spring_yod4"
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_spring_yod+5"] <- "VPD_spring_yod5"
+
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_summer_yod+1"] <- "VPD_summer_yod1"
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_summer_yod+2"] <- "VPD_summer_yod2"
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_summer_yod+3"] <- "VPD_summer_yod3"
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_summer_yod+4"] <- "VPD_summer_yod4"
+colnames(recovery_2608_1)[colnames(recovery_2608_1) == "VPD_summer_yod+5"] <- "VPD_summer_yod5"
+
+### write
+write.csv(recovery_2608_1, "~/eo_nas/EO4Alps/00_analysis/_recovery/recovery_2608.csv", row.names=FALSE)
+
+# Remove rows where recovery_rate > 38
+recovery_2608_filt <- recovery_2608_1[recovery_2608_1$recovery_rate <= 38, ]
+
+
+
+recovery_2608_unique <- recovery_2608_filt  %>%
   distinct(ID_new, .keep_all = TRUE) 
 
-# Step 1: Filter out rows where yod is less than 1987
-recovery_VPD_long1_filt <- recovery_VPD_long1  %>%
-  filter(yod >= 1986)
+recovery_2308_unique <- recovery_2308  %>%
+  distinct(ID, .keep_all = TRUE) 
 
-# Step 2: Create the new columns for VPD values corresponding to yod, yod + 1, yod + 2, and yod + 3
-recovery_VPD_long1_filt <- recovery_VPD_long1_filt %>%
-  group_by(ID_new) %>%
-  mutate(
-    VPD_yod = ifelse(year == yod, VPD_value, NA),
-    VPD_yod1 = ifelse(year == yod + 1, VPD_value, NA),
-    VPD_yod2 = ifelse(year == yod + 2, VPD_value, NA),
-    VPD_yod3 = ifelse(year == yod + 3, VPD_value, NA)
-  ) 
+# Select the specific columns from df1
+df_selected <- recovery_2308_unique[, c("ID", "agent", "agent_name", "geoloc")]
+
+# Merge df1_selected with df2 based on the 'ID' column
+recovery_2608_filt1 <- merge(recovery_2608_filt, df_selected, by = "ID", all.x = TRUE)
+
+### write
+write.csv(recovery_2608_filt1, "~/eo_nas/EO4Alps/00_analysis/_recovery/recovery_2608_filt_geoloc.csv", row.names=FALSE)
 
 
-
-
-# Step 2: Create a scatterplot
-ggplot(recovery_VPD_unique_filt, aes(x = VPD_value, y = recovery_rate)) +
+# Create a scatterplot
+ggplot(
+  data = recovery_2608_filt1 %>%
+    filter(
+      severity_class == "stand-replacing" &
+        !is.na(severity_class)),
+      aes(x = VPD_summer_yod1, y = recovery_rate)) +
   geom_point() +
   labs(title = "",
-       x = "VPD_value",
+       x = "VPD anomalies 1-year post-disturbance",
        y = "Recovery Rate") +
   theme_minimal()
+
+
+
+ggplot(
+  data = recovery_2608_filt1 %>%
+    filter(
+      severity_class == "stand-replacing" &
+        !is.na(geoloc)),
+      #) %>%
+    #filter(
+      #yod %in% c(2001, 2002, 2003, 2004)
+      #), 
+  aes(
+    y = recovery_rate, 
+    x =`VPD_absolute_yod1`, 
+    color = factor(yod)
+    )
+  ) +
+  geom_point() +
+  facet_wrap(~geoloc, scales = "free_y") +
+  labs(title = "",
+       x = "absolute VPD 1-year post-disturbance",
+       y = "Recovery Rate") 
+  #geom_smooth(
+    #method = "gam",
+    #se = FALSE
+    #)
+
+
+ggplot(
+  data = recovery_2608_filt1 %>%
+    filter(
+      severity_class == "stand-replacing" &
+        !is.na(geoloc)),
+  aes(
+    y = recovery_rate, 
+    x =`VPD_absolute_yod1`, 
+    color = cut(height, c(0, 1000, 2000, 3000))
+    )
+  ) +
+  geom_point() +
+  facet_wrap(cut(height, c(0, 1000, 2000, 3000))~geoloc) +
+  geom_smooth(
+    method = "gam",
+    formula = y ~ s(x)
+  )
+
+
+
+
 
 
